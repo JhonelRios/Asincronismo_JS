e28a12f868bbb1157ee4ad07539a37e0f8fab811
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2020-02-11 22:30:17 -0500

    Creacion de las funciones para acceder a los 3 datos que queremos haciendo callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,7 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
+const API = "https://rickandmortyapi.com/api/character/"
+
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest()
 
@@ -17,3 +19,16 @@ function fetchData(url_api, callback) {
 
     xhttp.send()
 }
+
+fetchData(API, (error1, data1) => {
+    if (error1) return console.error(error1)
+    fetchData(API + data1.results[0].id, (error2, data2) => {
+        if (error2) return console.error(error2)
+        fetchData(data2.origin.url, (error3, data3) => {
+            if (error3) return console.error(error3)
+            console.log(data1.info.count)
+            console.log(data2.name)
+            console.log(data3.dimension)
+        })
+    })
+})
