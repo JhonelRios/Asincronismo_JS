b25b2063ad4064ad8eb778354c936530a0ea4b86
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2020-02-11 23:38:46 -0500

    Creacion del archivo challenge.js y adecuar la funcion fetchData a ES6

--- src/promise/index.js
+++ src/promise/index.js
@@ -11,3 +11,22 @@ const somethingWillHappen = () => {
 somethingWillHappen()
     .then(response => console.log(response))
     .catch(error => console.error(error))
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => resolve("True"), 2000)
+        } else {
+            const error = new Error("Opps...")
+            reject(error)
+        }
+    })
+}
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err))
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => console.log(response))
+    .catch(err => console.error(err))
